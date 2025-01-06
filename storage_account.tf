9e05230eaf443538e64ac1d69c9a4da89ae5491b
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2025-01-06 14:09:29 -0300

    25. Referenciando Atributos de outros Blocos

--- storage_account.tf
+++ storage_account.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = ""
-  resource_group_name      = ""
+  resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
@@ -17,5 +17,5 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_container" "container" {
   name               = ""
-  storage_account_id = ""
+  storage_account_id = azurerm_storage_account.storage_account.id
 }
