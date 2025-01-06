798276a812d9b08bc4d77699a1629d69bb487a12
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2025-01-06 14:15:07 -0300

    26. Local Values

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  commom_tags = {
+    owner = "m0rb1u5"
+    managed-by = "terraform"
+  }
+}
--- storage_account.tf
+++ storage_account.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "resource_group" {
   name     = ""
   location = var.location
 
-  tags = ""
+  tags = local.commom_tags
 }
 
 resource "azurerm_storage_account" "storage_account" {
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  tags = ""
+  tags = local.commom_tags
 }
 
 resource "azurerm_storage_container" "container" {
